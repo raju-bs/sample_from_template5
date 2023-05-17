2850cb199b332f0c471a6d08716043e2e6ea2e5d
raju-kamble <125890460+raju-kamble@users.noreply.github.com>
2023-05-17 12:44:41 +0530

    Create vulnerable.py

---
+++ vulnerable.py
@@ -0,0 +1,21 @@
+// vulnerable code
+let username = request.params.get('username');
+let password = request.params.get('password');
+
+// check for valid input
+if (!username || !password) {
+	// return error
+	return response.status(400).send("Error: Invalid username or password");
+}
+
+// query database
+let queryString = `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`;
+let results = await db.query(queryString);
+
+// return results if valid
+if (results.length > 0) {
+	return response.status(200).send(results);
+} else {
+	// return error
+	return response.status(400).send("Error: Invalid username or password");
+}
