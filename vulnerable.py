3e4563bf431e74cbe9e6b6ab22aea15de8ee29a7
raju-bs <108449574+raju-bs@users.noreply.github.com>
2023-05-22 16:52:31 +0530

    Update vulnerable.py

--- vulnerable.py
+++ vulnerable.py
@@ -10,3 +10,4 @@ input_location = 'C:\uptycs\githubquery\gitquery_arun\gittest\sample.py'
 split_list = os.path.split(input_location)
 print("split list: {}".format(split_list))
 AWS_CREDENTIALS = { 'key': 'djekjdkejde', 'secret': 'dncndmncdmncd' }
+
